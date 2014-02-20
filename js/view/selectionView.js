c1707aa6989b5d117074094816939581bfbe3f82
henrilouis <h.l.schroter@gmail.com>
2014-02-20 11:11:40 +0100

    Fixed bugs.

--- js/view/selectionView.js
+++ js/view/selectionView.js
@@ -8,8 +8,6 @@ var SelectionView = function (container,model) {
 	// Get all the relevant elements of the view (ones that show data
   	// and/or ones that responed to interaction)
 	this.numberOfGuests = container.find("#numberOfGuests");
-	this.plusButton = container.find("#plusGuest");
-	this.minusButton = container.find("#minusGuest");
 	
 	//Creating the components dynamically. Here we create the following HTML content:
 	//
@@ -20,7 +18,47 @@ var SelectionView = function (container,model) {
 	//and add it to the the SelectionView 
 	
 	//div we just store in temporary variable because we won't need it later
-	var div = $("<div>");
+	var div = $("<div class='row'>");
+	var left = $("<div class='col-md-2'>");
+	var right = $("<div class='col-md-10'>");
+
+	var peopleBox = $("<div>");
+	var plusButton = $("<button class='btn'");
+	var minusButton = $("<button class='btn'");
+	var numberOfGuests = $("<span>");
+	var menuBox = $("<table class='table table-striped'>");
+	var confirmButton = $("<button class='btn'>");
+
+	var searchBox = $("<div>");
+	var searchInput = $("<input type='search'>");
+	var searchButton = $("<button class='btn'>");
+	var searchDropDopwn = $("<select>");
+	var dishBox = $("<div>");
+
+	peopleBox.html("<h3>My Dinner</h3>");
+	peopleBox.append(minusButton);
+	peopleBox.append(numberOfGuests);
+	peopleBox.append(plusButton);
+
+	menuBox.append("<tr><td>Dish Name</td><td>Cost</td></tr>");
+	menuBox.append("<tr><td></td><td>SEK 0.00</td></tr>");
+	menuBox.append(confirmButton);
+	
+	searchBox.html('SELECT DISH');
+	searchBox.append(searchInput);
+	searchBox.append(searchButton);
+	searchDropDown.append("<option>Starter</option><option>Main</option><option>Dessert</option>");
+	searchBox.append(searchDropDown);
+
+	left.append(peoplebox);
+	left.append(menubox);
+
+	right.append(searchBox);
+
+	div.append(left);
+	div.append(right);
+
+	/*
 	//we set the constant text
 	div.html("Total menu price ");
 	//and we add the text-primary class to make it blue
@@ -37,6 +75,8 @@ var SelectionView = function (container,model) {
 	//Set the inital values of the components
 	this.numberOfGuests.html(model.getNumberOfGuests());
 	this.totalPrice.html(model.getTotalMenuPrice());
+
+	*/
 	
 	/*****************************************  
 	      Observer implementation    
