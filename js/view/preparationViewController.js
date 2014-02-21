7569ec17f4daf7e794a364135e3b085b14e0b7f0
andreabeilei <andreabeilei@hotmail.com>
2014-02-21 15:54:11 +0100

    preparation.html/js updating
    not finished

--- js/view/preparationViewController.js
+++ js/view/preparationViewController.js
@@ -1,11 +1,17 @@
 //PreparationViewController Object constructor
 var PreparationViewController = function(view, model ) {
 	
-	view.plusButton.click(function(){
+
+	/*view.plusButton.click(function(){
 		model.setNumberOfGuests(model.getNumberOfGuests() + 1);
 	});
 	
 	view.minusButton.click(function(){
 		model.setNumberOfGuests(model.getNumberOfGuests() - 1);
+	});*/
+    
+    view.GoGackEditButton.click(function(){
+		window.stage("selectionView");
 	});
 }
+ // call update function to update number of guests; and initialize?
