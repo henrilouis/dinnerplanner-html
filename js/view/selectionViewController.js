db6b7f7187fca4b656d6787fa7669ebe454dee7e
aw-jansen <a.w.jansen.1988@gmail.com>
2014-02-21 16:25:52 +0100

    updated selection controller

--- js/view/selectionViewController.js
+++ js/view/selectionViewController.js
@@ -30,33 +30,4 @@ var SelectionViewController = function(view, model ) {
 			model.addDishToMenu(ui.draggable.attr('value'));
 		}
 	});
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-	$("figure").draggable(
-	{
-		appendTo:"body",
-		helper:"clone",
-		start:function(event,ui)
-		{
-			$(ui.helper).addClass('ui-draggable-helper');
-		}
-	});
-	$("figure").click(function()
-	{
-		if($(this).is('.ui-draggable-helper')) 
-		{
-        	return;
-	    }
-	    else
-	    {
-	    	window.currentDish = $(this).attr('value');
-	    	window.stage("dishView");
-	    }
-	    
-	});
-=======
->>>>>>> 76f07572791366ae2a095e736a34b3cb638e86cc
-=======
->>>>>>> 76f07572791366ae2a095e736a34b3cb638e86cc
 }
