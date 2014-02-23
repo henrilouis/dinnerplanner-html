a06de73ebf5d8b44477963486ea28ad3b31127c0
henrilouis <h.l.schroter@gmail.com>
2014-02-23 12:10:40 +0100

    Final tiny adjustments
    w00p

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -3,10 +3,6 @@ var DinnerModel = function() {
  
 	var menu = [];
 	var numberOfGuests =3; //set default number of guests
-	menu['starter'] = 1; //set a starter to the menu, to use for testing
-	menu['desert'] = 200; //set a starter to the menu, to use for testing
-	menu['main'] = 100; //set a starter to the menu, to use for testing
-
 
 	this.setNumberOfGuests = function(num) {
 		if(num>0) {
